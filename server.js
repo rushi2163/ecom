fa50dd5bf674c185390d41a4d077626d6f700b8b
rushi2163 <97241115+rushi2163@users.noreply.github.com>
2024-05-01 17:17:48 +0530

    Delete server.js

--- server.js
+++
@@ -1,8 +0,0 @@
-const express=require('express');
-const app=express();
-const jsonparse = require('body-parser')
-
-const userRoute=require('./routes/user')
-app.use(jsonparse.json())
-app.use('/user',userRoute);
-app.listen(3000);
