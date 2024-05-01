b80050f90816a85aec461e750b7c28010e90965b
rushi2163 <97241115+rushi2163@users.noreply.github.com>
2024-05-01 17:17:10 +0530

    Delete database.js

--- database.js
+++
@@ -1,9 +0,0 @@
-const mySql=require('mysql');
-var connection = mySql.createConnection(
-    {
-        host:'localhost',
-        user:'root',
-        password:'7410135881'
-    }
-)
-module.exports =connection;
